75c7f9115b027be17674135d475555ec79c74c75
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-17 16:01:24 +0500

    Task Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'block-task';
+export class AppComponent implements OnInit {
+
+  itemForm!: FormGroup;
+  items: any = ['hello', 'world', 'i', 'am', 'ahad'];
+  count: number = 0;
+
+  ngOnInit(): void {
+    this.count = this.items.length;
+    this.itemForm = new FormGroup({
+      text: new FormControl(' ', Validators.required)
+    })
+  }
+
+  addItem() {
+    if (this.itemForm.invalid) {
+      alert('Invalid input.');
+    }
+    else {
+      let ctrl = this.itemForm.controls;
+      console.log(ctrl['text'].getRawValue());
+      this.items.push(ctrl['text'].getRawValue());
+      this.itemForm.reset();
+      this.count++;
+    }
+  }
+
+  deleteItem(index: any) {
+    this.items.splice(index, 1);
+    this.count--;
+  }
+
+  getColor() {
+    let str = `rgb(${Math.floor(Math.random() * 255)},
+    ${Math.floor(Math.random() * 255)},
+    ${Math.floor(Math.random() * 255)}`; 
+    return str;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
